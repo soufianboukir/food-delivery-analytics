490e5b2b79a9848fd8e2f7f0ba2436a9cd889775
soufian <soufianeboukir0@gmail.com>
2026-05-25 19:56:31 +0100

    from localhost to public-ip

--- app/pages/05_predictions.py
+++ app/pages/05_predictions.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://localhost:5000"
+API_URL = "http://13.53.39.236:5000"
 
 st.set_page_config(page_title="Delivery ML System", layout="wide", page_icon="🔎")
 
